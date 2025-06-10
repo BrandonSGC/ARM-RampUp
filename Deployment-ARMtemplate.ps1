6c4ae5853d8b7cb2170e4b1d578401ec4781262d
BrandonGC <brangmz20@gmail.com>
2025-06-10 12:12:10 -0600

    Adding otuput messages

--- Deployment-ARMtemplate.ps1
+++ Deployment-ARMtemplate.ps1
@@ -5,13 +5,16 @@ $rg = "RG-ARM-ramp-up"
 
 # Remove Resource group if it already exists
 if (Get-AzResourceGroup -Name $rg -ErrorAction SilentlyContinue) {
+  Write-Output "Resource group $rg already exists. Deleting it..."
   Remove-AzResourceGroup -Name $rg -Force
 }
 
 # Create a new Resource group
+Write-Output "Creating resource group $rg..."
 New-AzResourceGroup -Name $rg -Location "Canada Central"
 
 # Deploy the ARM template
+Write-Output "Deploying ARM template"
 New-AzResourceGroupDeployment `
   -Name "arm-deployment" `
   -ResourceGroupName $rg `
