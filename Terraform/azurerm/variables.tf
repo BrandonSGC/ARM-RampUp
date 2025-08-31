40827d9f04797ebe7d9110fe612c0b5393433e88
BrandonSGC <brangmz20@gmail.com>
2025-08-31 12:41:00 -0600

    feat: adding variables

---
+++ Terraform/azurerm/variables.tf
@@ -0,0 +1,8 @@
+variable "location" {
+  description = "Azure region to deploy resources"
+  default     = "Canada Central"
+}
+
+variable "resource_group_name" {
+  default = "rg-terraform-infra"
+}
